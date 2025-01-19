c567d8c99857d345be7201a3b88aa0a90cf64b74
DennisY888 <dennisyang151@gmail.com>
2025-01-18 22:07:51 -0800

    configured models.py

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,11 +1,8 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from config import Config
-from flask_jwt_extended import JWTManager
-
-db = SQLAlchemy()
-jwt = JWTManager()
+from app.extensions import db, jwt
+from app.models import Preference
 
 def create_app():
     app = Flask(__name__)
@@ -17,8 +14,20 @@ def create_app():
     @app.shell_context_processor
     def make_shell_context():
         return {'db': db, 'app': app}
-    
-    from app import models
+        
     from app.routes import api
     app.register_blueprint(api)
+    
+    with app.app_context():
+        db.create_all()
+        if not Preference.query.first():
+            preferences = [
+                'vegetarian', 'halal', 'kosher', 'gluten_free', 'lactose_free',
+                'nut_free', 'soy_free', 'pescatarian', 'vegan', 'jain',
+                'hindu_vegetarian', 'low_sugar', 'flexitarian', 'raw_vegan', 'low_sodium'
+            ]
+            for pref in preferences:
+                db.session.add(Preference(preference=pref))
+            db.session.commit()
+            
     return app
