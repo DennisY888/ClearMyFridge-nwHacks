d20608a0991891e9a14cd816be687e0627738014
DennisY888 <dennisyang151@gmail.com>
2025-01-18 23:22:49 -0800

    fixed db

--- backend/config.py
+++ backend/config.py
@@ -5,7 +5,7 @@ basedir = path.abspath(path.dirname(__file__))
 
 class Config:
     SECRET_KEY = environ.get('SECRET_KEY', 'dev-key')
-    SQLALCHEMY_DATABASE_URI = environ.get('DATABASE_URL', f"sqlite:///{path.join(path.dirname(basedir), 'instance', 'dev.db')}")
+    SQLALCHEMY_DATABASE_URI = environ.get('DATABASE_URL', f"sqlite:///{path.join(basedir, 'dev.db')}")
     if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
         SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
